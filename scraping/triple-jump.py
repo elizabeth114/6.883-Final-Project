feb51b5b56755fa176a2d56243649b682deb38bb
Elizabeth <elizabethweeks22@gmail.com>
2020-05-05 20:07:20 -0700

    implemented scraping and did scraping for olympic and world athletics results

--- scraping/triple-jump.py
+++ scraping/triple-jump.py
@@ -4,13 +4,6 @@ import json
 import csv
 import sys
 
-def floatify(num):
-	try:
-		x = float(num)
-		return x
-	except Exception as e:
-		return num
-
 base_url = "https://www.olympic.org/"
 url_options = ["rio-2016", "london-2012", "beijing-2008", "athens-2004", "sydney-2000", "atlanta-1996"]
 url_end = "/athletics/triple-jump-women"
@@ -27,8 +20,12 @@ for year in url_options:
 	athletes = []
 	for index, person in enumerate(tree.xpath("//div[@class='main-holder']")[0].xpath("//div[@class='profile-section']")):
 		info = person.text_content().strip().split("\n")
+		split_name = info[0][:-1].split()
+		first = split_name[0]
+		last = " ".join(split_name[1:])
 		athlete = {
-		"name": info[0][:-1],
+		"first name": first,
+		"last name" : last,
 		"games": year,
 		"country": info[-1][-3:],
 		"distance": person.xpath("//td[@class='col3']")[index].text_content()[2:7]
@@ -36,6 +33,7 @@ for year in url_options:
 		game["athletes"].append(athlete)
 		if index==11:
 			break
+
 	games.append(game)
 f = open("results.json", "w")
 f.write(json.dumps(games, indent=4, sort_keys=True))
