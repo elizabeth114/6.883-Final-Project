ad9977a8acd20517dea0287ee241d01d5b7bc095
Patricia Lu <pjlu@mit.edu>
2020-05-11 16:19:02 -0500

    Allow training with >1 features (added months since last olympics)

--- Models/convert_data.py
+++ Models/convert_data.py
@@ -3,10 +3,11 @@ import csv
 import datetime
 
 from math import sqrt
+import numpy as np
 from numpy import concatenate
 from matplotlib import pyplot
 from pandas import read_csv
-from pandas import DataFrame
+from pandas import DataFrame, Series
 from pandas import concat
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import LabelEncoder
@@ -18,29 +19,33 @@ from keras.layers import LSTM
 def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
 	n_vars = 1 if type(data) is list else data.shape[1]
 	df = DataFrame(data)
-	cols, names = list(), list()
+	cols, names, cols2 = list(), list(), list() # cols2 to handle >1 features
 	# input sequence (t-n, ... t-1)
 	for i in range(n_in, 0, -1):
-		cols.append(df.shift(i))
-		names += [('var%d(t-%d)' % (j+1, i)) for j in range(n_vars)]
+		# cols.append(df.shift(i))
+		cols2.append(DataFrame(df.shift(i).values.flatten('F')))
+		names += [('(t-%d)' % (i))] # edited by pat
 	# forecast sequence (t, t+1, ... t+n)
 	for i in range(0, n_out):
-		cols.append(df.shift(-i))
+		# cols.append(df.shift(-i))
+		cols2.append(DataFrame(df.shift(-i).values.flatten('F')))
 		if i == 0:
-			names += [('var%d(t)' % (j+1)) for j in range(n_vars)]
+			names += [('(t)')]
 		else:
-			names += [('var%d(t+%d)' % (j+1, i)) for j in range(n_vars)]
+			names += [('(t+%d)' % (i))]
 	# put it all together
-	agg = concat(cols, axis=1)
+	# agg = concat(cols, axis=1)
+	agg = concat(cols2, axis=1)
 	agg.columns = names
 	# drop rows with NaN values
 	if dropnan:
 		agg.dropna(inplace=True)
-	return agg
+	return agg, n_vars # edited by pat
 
 def do_the_stuff():
 	# load dataset
 	dataset = read_csv('csv_dates.csv', header=0, index_col=0)
+	# print(dataset)
 	values = dataset.values
 	# integer encode direction
 	encoder = LabelEncoder()
@@ -50,20 +55,33 @@ def do_the_stuff():
 	scaler = MinMaxScaler(feature_range=(0, 1))
 	scaled = scaler.fit_transform(values)
 	# frame as supervised learning
-	reframed = series_to_supervised(values, 9, 1)
+	reframed, n_vars = series_to_supervised(values, 9, 1)
 	# drop columns we don't want to predict
 
 	# split into train and test sets
 	values = reframed.values
+	values = values.T
+	values = values.reshape((values.shape[0], n_vars, int(values.shape[1]/n_vars))).transpose((2,0,1)) # edited by pat, turns into (samples, timesteps, features)
+	# print(values)
+	# print(values.shape)
+
 	n_train_hours = len(values) - 30
-	train = values[:n_train_hours, :]
-	test = values[n_train_hours:, :]
+	train = values[:n_train_hours]
+	test = values[n_train_hours:]
+	# print('trainshape', train.shape)
+
 	# split into input and outputs
-	train_X, train_y = train[:, :-1], train[:, -1]
-	test_X, test_y = test[:, :-1], test[:, -1]
+	# train_X, train_y = train[:, :-1], train[:, -1] # original
+	# test_X, test_y = test[:, :-1], test[:, -1]
+	train_X, train_y = train[:,:-1,:], train[:,-1,0]
+	test_X, test_y = test[:,:-1,:], test[:,-1,0]
+
 	# reshape input to be 3D [samples, timesteps, features]
-	train_X = train_X.reshape((train_X.shape[0], 1, train_X.shape[1]))
-	test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
+	# train_X = train_X.reshape((train_X.shape[0], 1, train_X.shape[1])) # original
+	# test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
+	# train_X = train_X.reshape((train_X.shape[0], train_X.shape[1], 1)) # swap timesteps and features
+	# print(train_X[0])
+	# test_X = test_X.reshape((test_X.shape[0], test_X.shape[1], 1))
 	print(train_X.shape, train_y.shape, test_X.shape, test_y.shape)
 
 	model = Sequential()
@@ -82,18 +100,23 @@ def do_the_stuff():
 	# pyplot.legend()
 	# pyplot.show()
 	yhat = model.predict(test_X)
+	print(yhat.shape)
 
-	test_X = test_X.reshape((test_X.shape[0], test_X.shape[2]))
-	# invert scaling for forecast
-	inv_yhat = concatenate((yhat, test_X[:, 1:]), axis=1)
-	inv_yhat = scaler.inverse_transform(inv_yhat)
-	inv_yhat = inv_yhat[:,0]
-	# invert scaling for actual
-	test_y = test_y.reshape((len(test_y), 1))
-	inv_y = concatenate((test_y, test_X[:, 1:]), axis=1)
-	inv_y = scaler.inverse_transform(inv_y)
-	inv_y = inv_y[:,0]
+	# test_X = test_X.reshape((test_X.shape))#[0], test_X.shape[1]))
+	# print(test_X.shape)
+	# # invert scaling for forecast
+	# inv_yhat = concatenate((yhat, test_X[:, 1:]), axis=1) # am i changing this correctly?
+	# print(inv_yhat.shape)
+	# inv_yhat = scaler.inverse_transform(inv_yhat)
+	# inv_yhat = inv_yhat[:,0]
+	# # invert scaling for actual
+	# test_y = test_y.reshape((len(test_y), 1))
+	# inv_y = concatenate((test_y, test_X[:, 1:]), axis=1)
+	# inv_y = scaler.inverse_transform(inv_y)
+	# inv_y = inv_y[:,0]
 	# calculate RMSE
-	rmse = sqrt(mean_squared_error(inv_y, inv_yhat))
+	# rmse = sqrt(mean_squared_error(inv_y, inv_yhat))
+	
+	rmse = sqrt(mean_squared_error(test_y, yhat))
 	print('Test RMSE: %.3f' % rmse)
 	return yhat
--- Models/time_series.py
+++ Models/time_series.py
@@ -1,6 +1,7 @@
 import json
 import csv
 import datetime
+from datetime import timedelta
 
 months = {"jan": 1,
             "feb": 2,
@@ -21,16 +22,23 @@ def load_athlete(athlete_name, olympic_date):
             data = json.load(json_file)
             for athlete in data:
                 if athlete["first name"].lower() == athlete_name[0].lower() and athlete["last name"].lower() == athlete_name[1].lower():
-                    fieldnames = ['date', 'distance']
+                    fieldnames = ['date', 'distance', 'mos_since_oly']
                     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
                     writer.writeheader()
                     data = []
                     for year in athlete["results by year"]:
                         for result in year["results"]:
                             date = result["date"]
-                            date = datetime.datetime(int(year["year"]), months[date[3:6].lower()], int(date[:2]))
+                            year_value = int(year["year"])
+                            month_value = months[date[3:6].lower()]
+                            if year_value % 4 > 0:
+                                last_olympics_year = year_value - (year_value % 4)
+                            else:
+                                last_olympics_year = year_value if month_value > 8 else (year_value - 4) # olympics mid-August
+                            date = datetime.datetime(year_value, months[date[3:6].lower()], int(date[:2]))
+                            mos_since_last_olympics = (year_value - last_olympics_year) * 12 + (month_value - 8)
                             if date <   olympic_date:
-                                data.append({'date': date, 'distance': float(result["result"])})
+                                data.append({'date': date, 'distance': float(result["result"]), 'mos_since_oly': mos_since_last_olympics})
                     data = sorted(data, key = lambda dic: dic["date"])
                     writer.writerows(data)
                     print("loaded ", athlete["first name"], " ", athlete["last name"], "\'s data")
